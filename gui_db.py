948838640c082ea60c4f297632186bbc5759dba4
UltimaQuazar <gcw66@protonmail.com>
2018-03-15 16:20:26 -0500

    update: possible revamp with importing sql

--- gui_db.py
+++ gui_db.py
@@ -6,3 +6,9 @@ from wx.lib.mixins.listctrl import CheckListCtrlMizin, ListCtrlAutoWidthMixin
 class MainWindow(wx.Frame):
     def __init__(self, *args, **kwargs)
         wx.Dialog.__init__(self, None, title="Records", size=(600,400))
+        
+        info_label = wx.StaticText(self, label='Please Enter INFO below')
+        self.first_name wx.TextCtrl(self, -1, '')
+        wx.StaticText(self, -1, 'First Name')
+
+        
