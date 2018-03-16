b713798a146ef97a57edf43df81192dd31dbe9c6
UltimaQuazar <gcw66@protonmail.com>
2018-03-16 11:02:18 -0500

    Dialog and display building

--- gui_db.py
+++ gui_db.py
@@ -6,9 +6,36 @@ from wx.lib.mixins.listctrl import CheckListCtrlMizin, ListCtrlAutoWidthMixin
 class MainWindow(wx.Frame):
     def __init__(self, *args, **kwargs)
         wx.Dialog.__init__(self, None, title="Records", size=(600,400))
-        
+        app = wx.App()
+        window = wx.Frame(None, title = "", size=(something))
+
+        '''
+        below are the labels and buttons that will be displayed on the gui
+        '''
         info_label = wx.StaticText(self, label='Please Enter INFO below')
-        self.first_name wx.TextCtrl(self, -1, '')
+
+        '''
+        first name field
+        '''
+        self.first_name = wx.TextCtrl(self, -1, '')
         wx.StaticText(self, -1, 'First Name')
 
-        
+        '''
+        last name field
+        '''
+        self.last_name = wx.TextCtrl(self, -1, '')
+        wx.StaticText(self, -1, 'Last Name')
+
+        '''
+        Admission date
+        '''
+        self.admiss = (wx.TextCtrl(self, -1, '')
+        wx.StaticText(self, -1, 'Admission Date')
+
+        '''
+        Care term
+        '''
+        self.term = (wx.TextCtrl(self, -1, '')
+        wx.StaticText(self, -1, 'Term of Care')
+
+        wx.Display()
